ee7b9e4f3377042ed623ace8e7ee50d52369a80e
HectorUFRR <hector.ufrr@gmail.com>
2026-07-01 21:28:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ Diretrizes:
 
 # 3. Inicialização do modelo e chat
 if "gemini_model" not in st.session_state:
-    st.session_state.gemini_model = genai.GenerativeModel("
-        gemini-1.5-flash",
+    st.session_state.gemini_model = genai.GenerativeModel(
         model_name="gemini-1.5-flash-latest",
         system_instruction=system_instruction,
         generation_config={"temperature": 0.3}
